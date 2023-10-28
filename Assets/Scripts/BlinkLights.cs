517aba8759dfd2417bb5724ad6e93439e944a006
Prober76 <ugartegrover@gmail.com>
2023-10-28 11:03:56 -0500

    fix blinkLights

--- Assets/Scripts/BlinkLights.cs
+++ Assets/Scripts/BlinkLights.cs
@@ -5,90 +5,98 @@ using UnityEngine;
 public class BlinkLights : MonoBehaviour
 {
 
-	public Light[] right_lights = new Light[2];
-	public Light[] left_lights = new Light[2];
-	public KeyCode right_keyboard;
-	public KeyCode left_keyboard;
-	public KeyCode hazard_keyboard;
-	public float blink_time;
-	public float timer;
-	public bool toggle_right = false;
-	public bool toggle_left = false;
-	public bool toggle_hazard = false;
+    public Light[] right_lights = new Light[2];
+    public Light[] left_lights = new Light[2];
+    public KeyCode right_keyboard;
+    public KeyCode left_keyboard;
+    public KeyCode hazard_keyboard;
 
 
-	private void Start()
+    // Lights times
+    public float timer;
+    public float blink_time;
+    // Lights coordinator
+    public bool lights_on = false;
+    // Toggle lights
+    public bool toggle_right = false;
+    public bool toggle_left = false;
+    public bool toggle_hazard = false;
+
+
+    private void Start()
     {
-		timer = blink_time;
+        // Init timer
+        timer = blink_time;
     }
 
-    // Direccionales
-    void Blink_Light() {
-		if (timer > 0)
-		{
-			timer -= Time.deltaTime;
-		}
-		else {
-			if(toggle_right || toggle_hazard)
-				foreach (Light light in right_lights)
-				{
-					light.enabled = !light.enabled;
-				}
-			if(toggle_left || toggle_hazard)
-				foreach (Light light in left_lights)
-				{
-					light.enabled = !light.enabled;
-				}
-			timer = blink_time;
-		}
-	}
-
-	void Hazard_Lights_Blink() {
-		if (Input.GetKeyDown(hazard_keyboard)) {
-			toggle_hazard = !toggle_hazard;
-		}
-	}
-
-	void Right_Lights_Blink()
+    // Turn Lights on and off
+    void UpdateLights()
     {
-		if (Input.GetKeyDown(right_keyboard))
-		{
-			toggle_left = false;
-			foreach (Light light in left_lights)
-				light.enabled = false;
-			if (toggle_right)
-			{
-				foreach (Light light in right_lights)
-					light.enabled = false;
-			}
+        // Update lights
+        bool left_on = (toggle_left || toggle_hazard) && lights_on;
+        bool right_on = (toggle_right || toggle_hazard) && lights_on;
 
-			toggle_right = !toggle_right;
-		}
-	}
-	void Left_Lights_Blink()
-	{
-		if (Input.GetKeyDown(left_keyboard))
-		{
-			toggle_right = false;
-			foreach (Light light in right_lights)
-				light.enabled = false;
-			if (toggle_left)
-			{
-				foreach (Light light in left_lights)
-					light.enabled = false;
-			}
+        foreach (Light light in right_lights)
+        {
+            light.enabled = right_on;
+        }
+        foreach (Light light in left_lights)
+        {
+            light.enabled = left_on;
+        }
+    }
 
-			toggle_left = !toggle_left;
-		}
-	}
+    // Update timer
+    void Blink_Light()
+    {
+        if (timer > 0)
+        {
+            // Update timer
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            lights_on = !lights_on;
+            timer = blink_time;
+            UpdateLights();
+        }
+    }
 
-	void Update()
-	{
-		Right_Lights_Blink();
-		Left_Lights_Blink();
-		Hazard_Lights_Blink();
-		if(toggle_right || toggle_left || toggle_hazard)
-			Blink_Light();
-	
-	}
+    void Hazard_Lights_Blink()
+    {
+        if (Input.GetKeyDown(hazard_keyboard))
+        {
+            toggle_hazard = !toggle_hazard;
+            UpdateLights();
+        }
+    }
+
+    void Right_Lights_Blink()
+    {
+        if (Input.GetKeyDown(right_keyboard))
+        {
+            toggle_left = false;
+            toggle_right = !toggle_right;
+            UpdateLights();
+        }
+    }
+    void Left_Lights_Blink()
+    {
+        if (Input.GetKeyDown(left_keyboard))
+        {
+            toggle_right = false;
+            toggle_left = !toggle_left;
+            UpdateLights();
+        }
+    }
+
+    void Update()
+    {
+        Right_Lights_Blink();
+        Left_Lights_Blink();
+        Hazard_Lights_Blink();
+        if (toggle_right || toggle_left || toggle_hazard)
+            Blink_Light();
+
+    }
 }
