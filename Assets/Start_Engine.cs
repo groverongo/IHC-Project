62485bfdd6a2cd6e45b706e1f13df3607531306e
Prober76 <ugartegrover@gmail.com>
2023-11-27 18:26:58 -0500

    audio?

---
+++ Assets/Start_Engine.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlaySoundOnCreate : MonoBehaviour
+{
+    public AudioClip soundClip; // The audio clip to be played
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        Debug.Log("ENGINE ON");
+        // Add an AudioSource component to the GameObject
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        // Set the AudioClip to the AudioSource
+        audioSource.clip = soundClip;
+        audioSource.volume = 1;
+
+        // Play the sound
+        audioSource.Play();
+    }
+}
