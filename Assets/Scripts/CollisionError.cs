f7cf1d20536d698f7544d77eaf482c9c7ec9c7dc
Prober76 <ugartegrover@gmail.com>
2023-11-29 23:18:16 -0500

    alarma de errores

--- Assets/Scripts/CollisionError.cs
+++ Assets/Scripts/CollisionError.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class CollisionError : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // S
+    // tart is called before the first frame update
+
+    public AudioClip collision_sound;
+    public AudioSource collision_source;
+
+    void Start()
+    {
+        collision_source = gameObject.AddComponent<AudioSource>();
+        collision_source.clip = collision_sound;
+        collision_source.loop = true;
+    }
 
     void OnCollisionEnter(Collision collision)
     {
+
         Debug.Log("COLISIONES");
+        if (!collision_source.isPlaying)
+        {
+            collision_source.Play();
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision_source.isPlaying)
+        {
+            collision_source.Stop();
+        }    
     }
 }
